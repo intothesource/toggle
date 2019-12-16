b2e40ee6c89ee5c4d0e8832ad0850146a8bdbe4e
Ramon Valk <rvalk@intothesource.com>
2019-12-16 14:17:40 +0100

    Added toggle, functionality and styling

---
+++ toggle.js
@@ -0,0 +1,32 @@
+class Toggle extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        if (this.dataset.itsToggleName) {
+            this.createElements(this.dataset.itsToggleName);
+        } else {
+            const randomName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+            this.createElements(randomName);
+        }
+    }
+
+    createElements(toggleName) {
+        const inputElement = document.createElement('input');
+        const labelElement = document.createElement('label');
+
+        inputElement.setAttribute('type', 'checkbox');
+        inputElement.setAttribute('id', `its-toggle-${toggleName}`);
+        inputElement.setAttribute('name', `${toggleName}`);
+        inputElement.setAttribute('data-its-toggle-input', '');
+
+        labelElement.setAttribute('for', `its-toggle-${toggleName}`);
+        labelElement.setAttribute('data-its-toggle-label', '');
+
+        this.appendChild(inputElement);
+        this.appendChild(labelElement);
+    }
+}
+
+window.customElements.define('its-toggle', Toggle);
